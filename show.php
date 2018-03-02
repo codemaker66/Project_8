bd80748070436dffec3a1228e8186b228d5d0afa
codemaker66 <codemaker66@gmail.com>
2018-03-02 20:59:34 +0100

    Creating a template file for show and comments.php

--- show.php
+++ show.php
@@ -1,29 +1,8 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php $title = 'accueil'; ?>
 
-  <head>
+<?php ob_start(); ?>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Jean Forteroche Blog</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- Custom styles for this template -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <!-- Navigation -->
+<!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand" href="show.php">Le blog de Jean Forteroche</a>
@@ -34,7 +13,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="show.php">Home</a>
+              <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="about.html">About</a>
@@ -63,8 +42,8 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <?php
 
+<?php
 while ($data = $req->fetch())
 {
 ?>
@@ -83,8 +62,8 @@ while ($data = $req->fetch())
 } 
 $req->closeCursor();
 ?>       
-        
-        </div>
+
+</div>
       </div>
     </div>
 
@@ -107,6 +86,6 @@ $req->closeCursor();
     <!-- Custom scripts for this template -->
     <script src="js/clean-blog.min.js"></script>
 
-  </body>
+<?php $content = ob_get_clean(); ?>
 
-</html>
+<?php require('template.php'); ?>
